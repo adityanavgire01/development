830ae195ff54a4e9e06727a24b97f110dee2cf18
Aditya <aditya.p.navgire@gmail.com>
2025-02-18 09:48:01 +0530

    Add event listeners to cells and handle player moves

--- TheOdinProject/TicTacToe/script.js
+++ TheOdinProject/TicTacToe/script.js
@@ -1,5 +1,5 @@
 const board = document.getElementById("board");
-const cells = document.querySelectorAll('cell');
+const cells = document.querySelectorAll('.cell');
 const status = document.getElementById("status");
 const restartButton = document.getElementById("restartButton");
 
@@ -15,3 +15,22 @@ const winningCombinations = [
     ,[0, 3, 6], [1, 4, 7], [2, 5, 8] // columns
     ,[0, 4, 8], [2, 4, 6] //diagonals
 ]
+
+function handleCellClick(e) {
+    let cell = e.target;
+    let cellIndex = Array.from(cells).indexOf(cell);
+
+    if (gameState[cellIndex] !== '' || !gameActive) return;
+
+    gameState[cellIndex] = currentPlayer;
+    cell.textContent = currentPlayer;
+
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    status.textContent = `Player ${currentPlayer}'s turn`;
+    
+}
+
+// Event listener
+cells.forEach(cell => {
+    cell.addEventListener('click', handleCellClick)
+})
